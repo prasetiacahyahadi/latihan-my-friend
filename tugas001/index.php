5a274e4b3f8c9ae572004d7179d4439ebcad90c5
Prasetia Cahya Hadi <cahya.cahya.pc@gmail.com>
2024-12-11 00:59:17 +0700

    20241211_005901 add tugas 1

---
+++ tugas001/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    
+    <h1>tugas 1</h1>
+    <ol>
+        <li>
+            <b>Duis dictum ante urna</b>
+            <p>
+                fringilla condimentum dui scelerisque sit amet. 
+                <br> Proin est diam, congue at congue a, convallis vel lorem.
+            </p>
+        </li>
+    </ol>
+
+</body>
+</html>
